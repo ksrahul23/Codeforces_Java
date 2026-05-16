0ec97d39d714e87cc2408c7724ef37c921d77526
Rahul Kumar <ksrahul2305@gmail.com>
2026-05-16 20:10:57 +0530

    CODEFORCES

---
+++ BracketSequence.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class BracketSequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+            if (n % 2 != 0) {
+                System.out.println("NO");
+                continue;
+            }
+            
+            int openCount = 0;
+            int closeCount = 0;
+            for (int i = 0; i < n; i++) {
+                if (s.charAt(i) == '(') {
+                    openCount++;
+                } else {
+                    closeCount++;
+                }
+            }
+            if (openCount == closeCount) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
---
+++ CSnowfall.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class CSnowfall {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) return;
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            List<Integer> d6 = new ArrayList<>();
+            List<Integer> d2 = new ArrayList<>();
+            List<Integer> d3 = new ArrayList<>();
+            List<Integer> net = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                int v = sc.nextInt();
+                if (v % 6 == 0) d6.add(v);
+                else if (v % 2 == 0) d2.add(v);
+                else if (v % 3 == 0) d3.add(v);
+                else net.add(v);
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            // Try: 6s first, then 2s, then Neutrals (the wall), then 3s
+            for (int x : d6) sb.append(x).append(" ");
+            for (int x : d2) sb.append(x).append(" ");
+            for (int x : net) sb.append(x).append(" ");
+            for (int x : d3) sb.append(x).append(" ");
+            
+            System.out.println(sb.toString().trim());
+        }
+    }
+}
