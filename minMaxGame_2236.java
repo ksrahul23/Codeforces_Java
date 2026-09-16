b25dc92f79e2f12c1924ec8f82c0f01463011308
Rahul Kumar <ksrahul2305@gmail.com>
2026-09-16 23:46:22 +0530

    CODEFORCES

--- minMaxGame_2236.java
+++ minMaxGame_2236.java
@@ -1,24 +1,55 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class minMaxGame_2236 {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        FastReader in = new FastReader();
+        int t = in.nextInt();
+        StringBuilder out = new StringBuilder();
+        
         while (t-- > 0) {
-            int n = sc.nextInt();
+            int n = in.nextInt();
             int countOnes = 0;
             
             for (int i = 0; i < n; i++) {
-                int val = sc.nextInt();
-                if (val == 1) {
+                if (in.nextInt() == 1) {
                     countOnes++;
                 }
             }
-            if (countOnes >= 2 || (countOnes == 1 && n == 2)) {
-                System.out.println("Bessie");
+            int elsieTurns = (n - 1) / 2;
+            
+            if (countOnes > elsieTurns) {
+                out.append("Bessie\n");
             } else {
-                System.out.println("Elsie");
+                out.append("Elsie\n");
             }
         }
+        System.out.print(out);
     }
 }
